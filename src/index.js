9599055dfca4be51894e31ace979c08d2b5b8409
Ravi Roshan <raviroshan.talk@gmail.com>
2017-04-13 01:13:48 +0530

    Added support for iterating through Object's Array property using index.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ function getValue(obj, properties, defaultReturn, onlyOwnProperty) {
 	var res = defaultReturn;
 	var key, index;
 
+	var lastPosition;
+	var arrayIndexPosition = -1;
+
 	if (typeof obj !== 'object') {
 		throw new Error('Please pass a valid object as first parameter');
 	}
@@ -18,16 +21,24 @@ function getValue(obj, properties, defaultReturn, onlyOwnProperty) {
 
 	for (index = 0; index < propertyArray.length; index++) {
 		key = propertyArray[index];
+
+		// Check for the Property - Array or simple Key
+		if (key.endsWith(']')) {
+			lastPosition = key.lastIndexOf('[');
+			arrayIndexPosition = Number(key.substring(lastPosition + 1, key.length - 1));
+			key = key.substring(0, lastPosition);
+		}
+
 		// Check if the user wants to Get the Value of Property ONLY on the Current Object
 		// By default it checks prototype properties
 		if (onlyOwnProperty) {
 			if (appendObj.hasOwnProperty(key)) {
-				res = appendObj[key];
+				res = (arrayIndexPosition > -1) ? appendObj[key][arrayIndexPosition] : appendObj[key];
 			} else {
 				res = undefined;
 			}
 		} else {
-			res = appendObj[key];
+			res = (arrayIndexPosition > -1) ? appendObj[key][arrayIndexPosition] : appendObj[key];
 		}
 
 		// If Value is undefined, break the flow and return undefined
@@ -36,7 +47,8 @@ function getValue(obj, properties, defaultReturn, onlyOwnProperty) {
 			res = defaultReturn;
 			break;
 		} else {
-			appendObj = appendObj[key];
+			appendObj = (arrayIndexPosition > -1) ? appendObj[key][arrayIndexPosition] : appendObj[key];
+			arrayIndexPosition = -1;
 		}
 	}
 	return res;
--- test/index.js
+++ test/index.js
@@ -11,6 +11,20 @@ var Person = function() {
 		city: 'Default - New Delhi',
 		country: 'Default - India'
 	};
+	this.hobbies = [{
+		name: 'Singing',
+		type: 'Indoor'
+	}, {
+		name: 'Football',
+		type: 'Outdoor'
+	}, {
+		name: 'Dancing',
+		type: 'Outdoor'
+
+	}, {
+		name: 'Cooking',
+		type: 'Outdoor'
+	}];
 };
 
 var Employee = function() {
@@ -25,47 +39,99 @@ var Employee = function() {
 		designation: 'Consultant',
 		domain: 'e-commerce'
 	};
+	this.friends = [{
+		name: 'Siddant',
+		age: 27
+	}, {
+		name: 'Harsh',
+		age: 26
+	}, {
+		name: 'Stuti',
+		age: 30
+	}];
 };
 
+// Inherit the Person Prototype
 Employee.prototype = new Person();
 
+// Create instance of Employee
 emp1 = new Employee();
 
 describe('Object-getValue', function() {
 	describe('Check for property on Current Object', function() {
-		it('should return the name', function() {
-			assert.equal(getValue(emp1, 'name'), emp1.name);
+		describe('Key as Object Property', function() {
+			it('should return the name', function() {
+				assert.equal(getValue(emp1, 'name'), emp1.name);
+			});
+
+			it('should return the nested details.domain property', function() {
+				assert.equal(getValue(emp1, 'details.domain'), emp1.details.domain);
+			});
+
+			it('should return undefined - property not found', function() {
+				assert.equal(getValue(emp1, 'details.language.primary'), undefined);
+			});
 		});
 
-		it('should return the nested details.domain property', function() {
-			assert.equal(getValue(emp1, 'details.domain'), emp1.details.domain);
-		});
+		describe('Array as Object Property', function() {
+			it('should return second friend name property', function() {
+				assert.equal(getValue(emp1, 'friends[1].name'), 'Harsh');
+			});
 
-		it('should return undefined - property not found', function() {
-			assert.equal(getValue(emp1, 'details.language.primary'), undefined);
+			it('should return second friend complete Object', function() {
+				assert.equal(getValue(emp1, 'friends[1]'), emp1.friends[1]);
+			});
+
+			it('should return undefined - object not found', function() {
+				assert.equal(getValue(emp1, 'friends[5]'), undefined);
+			});
+
+			it('should return undefined - property not found', function() {
+				assert.equal(getValue(emp1, 'friends[5].name'), undefined);
+			});
 		});
 	});
 
 	describe('Check for property on Prototype Object', function() {
-		it('should return the age', function() {
-			assert.equal(getValue(emp1, 'age'), emp1.age);
+		describe('Key as Object Property', function() {
+			it('should return the age', function() {
+				assert.equal(getValue(emp1, 'age'), emp1.age);
+			});
+
+			it('should return undefined - property not found', function() {
+				assert.equal(getValue(emp1, 'address.area.pincode'), undefined);
+			});
+
+			it('should return undefined - property not found, even if the parent node is present child', function() {
+				assert.equal(getValue(emp1, 'address.country'), undefined);
+			});
 		});
 
-		it('should return undefined - property not found', function() {
-			assert.equal(getValue(emp1, 'address.area.pincode'), undefined);
-		});
+		describe('Array as Object Property', function() {
+			it('should return first hobby name property', function() {
+				assert.equal(getValue(emp1, 'hobbies[0].name'), emp1.hobbies[0].name);
+			});
 
-		it('should return undefined - property not found, even if the parent node is present child', function() {
-			assert.equal(getValue(emp1, 'address.country'), undefined);
+			it('should return first hobby complete Object', function() {
+				assert.equal(getValue(emp1, 'hobbies[0]'), emp1.hobbies[0]);
+			});
+
+			it('should return undefined - object not found', function() {
+				assert.equal(getValue(emp1, 'hobbies[5]'), undefined);
+			});
+
+			it('should return undefined - property not found', function() {
+				assert.equal(getValue(emp1, 'hobbies[5].name'), undefined);
+			});
 		});
 	});
 
 	describe('Check for property on Current Object - with Default Return', function() {
-		it('should return the Default Return - If not found', function() {
+		it('should return the Default Return [Type : String] - If not found', function() {
 			assert.equal(getValue(emp1, 'salary', 'Salary not found'), 'Salary not found');
 		});
 
-		it('should return the Default Return - If not found', function() {
+		it('should return the Default Return [Type : Number] - If not found', function() {
 			assert.equal(getValue(emp1, 'salary', 9999), 9999);
 		});
 
@@ -83,6 +149,14 @@ describe('Object-getValue', function() {
 	describe('Check for property ONLY on Current Object', function() {
 		it('should NOT return the age', function() {
 			assert.equal(getValue(emp1, 'age', 'Age not found', true), 'Age not found');
+		});
+
+		it('should NOT return first hobby name property', function() {
+			assert.equal(getValue(emp1, 'hobbies[0].name', 'Hobby not found', true), 'Hobby not found');
+		});
+
+		it('should return first friend name property', function() {
+			assert.equal(getValue(emp1, 'friends[0].name', 'Friend Name not found', true), emp1.friends[0].name);
 		});
 	});
 
